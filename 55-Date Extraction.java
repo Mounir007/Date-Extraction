6725c3e83caa8fc4237be5e7d7523bd0975ae34a
Mounir Hafsa <Mounirhafsa@gmail.com>
2018-09-04 14:39:50 +0200

    Update 55-Date Extraction.java

--- 55-Date Extraction.java
+++ 55-Date Extraction.java
@@ -115,6 +115,20 @@ public class FiFty {
 
 }
 
+    Text in Test.txt : 
+
+Marvin Lee Minsky at the 05/13/2017 Mathematics Genealogy Project; 20 May 2014
+Marvin Lee Minsky 07/09/2017 AI Genealogy Project. {reprint 18 September 2011)
+"Personal page for Marvin Minsky". web.media.mit.edu. Retrieved 23 June 2016.
+Admin (January 27, 2016). "Official AlcorMay 13, 2017 Statement Concerning Marvin Minsky". 
+Alcor Life Extension Foundation. Retrieved 2016-04-07.
+"IEEE Computer Society Magazine Honors 2015-02-13 Artificial Intelligence Leaders". 
+DigitalJournal.com. August 24, 2011. Retrieved September 18, 2011. 
+Press release source: PRWeb (Vocus).
+"Dan David prize 2014 winners". May 15, 2014. Retrieved May 20, 2014.
+
+    Output :
+                                                    
 Frequency of 2011-08-24 is 1
 Frequency of 2011-09-18 is 2
 Frequency of 2014-05-15 is 1
